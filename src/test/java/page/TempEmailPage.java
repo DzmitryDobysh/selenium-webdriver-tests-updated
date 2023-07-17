4a2b8a854d109b71f1147fa79aa7e851e4752e3a
Dzmitry Dobysh <Dzmitry_Dobysh@epam.com>
2023-07-17 19:03:15 +0400

    fix: added explicit wait for HardcoreTest

--- src/test/java/page/TempEmailPage.java
+++ src/test/java/page/TempEmailPage.java
@@ -3,7 +3,11 @@ package page;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import spec.BaseSpec;
+
+import java.time.Duration;
 
 public class TempEmailPage extends BaseSpec {
     //used another service instead of https://yopmail.com/en/ - probably got banned for no reason
@@ -31,6 +35,10 @@ public class TempEmailPage extends BaseSpec {
     }
 
     public String getCostFromEmail() {
-        return waitForWebElementVisible(priceAmount).getText();
+        Duration extendedTimeout = WAIT_TIMEOUT_SECONDS.plusSeconds(20);
+        WebDriverWait wait = new WebDriverWait(driver, extendedTimeout);
+        WebElement visibleElement = wait.until(ExpectedConditions.visibilityOf(priceAmount));
+        return visibleElement.getText();
     }
+
 }
